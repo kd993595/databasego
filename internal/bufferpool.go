a2d41c485a9ab4d66e0c6bc98a258c3975dba26f
kd993595 <kepedu17@gmail.com>
2024-03-19 18:40:10 -0400

    update bufferpool

--- internal/bufferpool.go
+++ internal/bufferpool.go
@@ -3,13 +3,15 @@ package internal
 import (
 	"crypto/md5"
 	"encoding/binary"
+	"errors"
+	"fmt"
 	"os"
 	"path/filepath"
 	"sync"
 	"sync/atomic"
 )
 
-type BufferPool interface {
+/*type BufferPool interface {
 	FetchPage(tablePos int, pageid PageID) *InternalPage
 	DeletePage(tableName string, pageid PageID)
 	GetPageDisk(pageid PageID) (*InternalPage, error) //disk manager
@@ -17,7 +19,7 @@ type BufferPool interface {
 	SelectDataRange(start PageID, end PageID) []*InternalPage
 	Unpin(pageid PageID)
 	Victim() int
-}
+}*/
 
 type PageID uint64
 
@@ -28,145 +30,162 @@ type InternalPage struct {
 	pinned   bool
 }
 
-type pageTable map[PageID]int
-
 type BufferPoolManager struct {
-	slots     [MAXPOOLSIZE]*InternalPage
-	freelist  []int
-	mx        sync.Mutex
-	pagemx    sync.RWMutex
-	dir       string
-	alltables []pageTable
+	dir      string
+	allpools map[string]bufferPool
+}
+
+type bufferPool struct {
+	slots          [MAXPOOLSIZE]*InternalPage
+	freelist       []int
+	mxread         *sync.Mutex
+	mxwrite        *sync.Mutex
+	pagemx         *sync.RWMutex
+	alltables      map[PageID]int
+	tablefileRead  *os.File
+	tablefileWrite *os.File
 }
 
 func NewBufferPool(dir string) *BufferPoolManager {
-	newPool := BufferPoolManager{
+	newManager := BufferPoolManager{
+		dir:      dir,
+		allpools: make(map[string]bufferPool),
+	}
+
+	return &newManager
+}
+
+func (bm *BufferPoolManager) NewPool(tablename string, dir string) {
+	newPool := bufferPool{
 		slots:    [MAXPOOLSIZE]*InternalPage{},
 		freelist: make([]int, MAXPOOLSIZE),
-		mx:       sync.Mutex{},
-		pagemx:   sync.RWMutex{},
-		dir:      dir,
+		mxread:   &sync.Mutex{},
+		mxwrite:  &sync.Mutex{},
+		pagemx:   &sync.RWMutex{},
 	}
 	for i := 0; i < MAXPOOLSIZE; i++ {
 		newPool.freelist[i] = i
 	}
-
-	return &newPool
-
+	for i := range newPool.slots {
+		newPool.slots[i] = &InternalPage{
+			buf:      [PAGESIZE]byte{},
+			pincount: atomic.Int32{},
+			id:       0,
+			pinned:   false,
+		}
+	}
+	filePathStr := filepath.Join(dir, tablename, ".db")
+	_, err := os.Stat(filePathStr)
+	if err != nil {
+		os.Create(filePathStr)
+	}
+	newPool.tablefileRead, _ = os.OpenFile(filePathStr, os.O_RDONLY, 0644)
+	newPool.tablefileWrite, _ = os.OpenFile(filePathStr, os.O_WRONLY, 0644)
+	bm.allpools[tablename] = newPool
 }
 
-func (b *BufferPoolManager) FetchPage(tablePos int, pageid PageID) *InternalPage {
+func (bm *BufferPoolManager) ManagerFetchPage(tablename string, pageid PageID) (*InternalPage, error) {
+	pool, ok := bm.allpools[tablename]
+	if !ok {
+		return nil, errors.New(fmt.Sprintf("table name: \"%s\" does not exist", tablename))
+	}
+	return pool.FetchPage(pageid), nil
+}
 
-	var tablemap pageTable = b.alltables[tablePos]
-	pagepos, ok := tablemap[pageid]
+// returns nil if error occured
+func (b *bufferPool) FetchPage(pageid PageID) *InternalPage {
+	b.pagemx.RLock()
+	pagepos, ok := b.alltables[pageid]
 	if ok {
 		tmppage := b.slots[pagepos]
 		tmppage.pincount.Add(1)
+		tmppage.pinned = true
+		b.pagemx.RUnlock()
 		return tmppage
 	}
-	//getframeid and have to allocate page from bufefr if none free
-	return nil
+	//getframeid and have to allocate page from buffer if none free
+	b.pagemx.RUnlock()
+	frameId := b.GetFrameID(pageid)
+	b.slots[frameId].pincount.Store(1)
+	b.slots[frameId].id = pageid
+	err := b.AllocatePage(pageid, frameId)
+	if err != nil {
+		return nil
+	}
+	return b.slots[frameId]
 }
 
-func (b *BufferPoolManager) GetFrameID() int {
+func (b *bufferPool) GetFrameID(pageid PageID) int {
+	b.pagemx.Lock()
+	defer b.pagemx.Unlock()
 	if len(b.freelist) > 0 {
 		frameID, newFreeList := b.freelist[0], b.freelist[1:]
 		b.freelist = newFreeList
+		b.slots[frameID].pinned = true
 		return frameID
 	}
 	//return clockreplacer scan through pages pincount
+	for {
+		frameID := 0
+		for i := range b.slots {
+			if !b.slots[i].pinned && b.slots[i].pincount.Load() <= 0 {
+				delete(b.alltables, pageid)
+				frameID = i
+				break
+			}
+
+		}
+		b.slots[frameID].pinned = true
+		return frameID
+	}
 }
 
-func (b *BufferPoolManager) GetPageDisk(pageid PageID) (*InternalPage, error) {
-	b.pagemx.Lock()
-	defer b.pagemx.Unlock()
+// read page from disk
+func (b *bufferPool) AllocatePage(pageid PageID, frameId int) error {
+	b.mxread.Lock()
+	defer b.mxread.Unlock()
 
-	slotID := 0
-	if len(b.freelist) > 0 {
-		slotID, b.freelist = b.freelist[0], b.freelist[1:]
-	} else {
-		slotID = b.Victim()
-	}
-	//read page from disk
-	filename := filepath.Join(b.dir, pageid.tableName, ".db")
-	f, err := os.Open(filename)
+	_, err := b.tablefileRead.Seek(int64(pageid)*PAGESIZE, 0)
 	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-	_, err = f.Seek(int64(pageid.pageNum)*PAGESIZE, 0)
-	if err != nil {
-		return nil, err
+		return err
 	}
 	buf := make([]byte, PAGESIZE)
-	_, err = f.Read(buf)
+	_, err = b.tablefileRead.Read(buf)
 	if err != nil {
-		return nil, err
+		return err
 	}
-	pageCache := &InternalPage{}
-	pageCache.buf = [4096]byte(buf)
-	pageCache.id = pageid
-	pageCache.pincount.Store(1)
-	b.slots[slotID] = pageCache
-	return pageCache, nil
+
+	b.slots[frameId].buf = [4096]byte(buf)
+	return nil
 }
 
-// only call in writer locked thread
-func (b *BufferPoolManager) Victim() int {
-	for {
-		for i, v := range b.slots {
-			if v == nil {
-				return i
-			} else {
-				if v.pincount.Load() <= 0 {
-					return i
-				} else {
-					v.pincount.Add(-1)
-				}
-			}
-		}
+func (b *bufferPool) Unpin(frameId int) {
+	b.slots[frameId].pincount.Add(-1)
+	if b.slots[frameId].pincount.Load() <= 0 {
+		b.slots[frameId].pinned = false
 	}
 }
 
-func (b *BufferPoolManager) Unpin(pageid PageID) {
-	b.pagemx.RLock()
-	defer b.pagemx.RUnlock()
-
-	for i, v := range b.slots {
-		if v != nil {
-			if pageid.Equals(v.id) {
-				b.slots[i].pincount.Add(-1)
-				return
-			}
-		}
-	}
-}
-
-func (b *BufferPoolManager) DeletePage(pageid PageID) {
-	b.pagemx.Lock()
-	for i, v := range b.slots {
-		if v != nil {
-			if pageid.Equals(v.id) {
-				b.slots[i] = nil
-				b.freelist = append(b.freelist, i)
-			}
-		}
-	}
-	b.pagemx.Unlock()
+func (bm *BufferPoolManager) UnpinPage(tablename string, frameid int) {
+	pool, _ := bm.allpools[tablename]
+	pool.Unpin(frameid)
 }
 
 // return last page modified
-func (b *BufferPoolManager) InsertData(pageid PageID, data [][]byte) (uint64, error) {
-	b.mx.Lock()
-	defer b.mx.Unlock()
+func (bm *BufferPoolManager) InsertData(tablename string, pageid PageID, data [][]byte) (uint64, error) {
 
-	pageToModify := b.FetchPage(pageid)
-	buf := pageToModify.buf
-
-	f, err := os.OpenFile(filepath.Join(b.dir, pageid.tableName, ".db"), os.O_WRONLY, 0666)
-	if err != nil {
-		return 0, err
+	pool, ok := bm.allpools[tablename]
+	if !ok {
+		return 0, errors.New(fmt.Sprintf("table name: \"%s\" does not exist", tablename))
 	}
+	pageToModify := pool.FetchPage(pageid)
+	if pageToModify == nil {
+		return 0, errors.New("internal error fetching page")
+	}
+	buf := pageToModify.buf //pointer
+	pool.mxwrite.Lock()
+	defer pool.mxwrite.Unlock()
+	f := pool.tablefileWrite
 
 	//numberOfPage := binary.LittleEndian.Uint64(buf[0:8])
 	rowNums := binary.LittleEndian.Uint16(buf[8:10])
@@ -174,11 +193,11 @@ func (b *BufferPoolManager) InsertData(pageid PageID, data [][]byte) (uint64, er
 
 	offset := 26 + int(rowNums)*len(data[0])
 	rows := 0
-	pgNum := pageid.pageNum
+	pgNum := pageid
 	for i := range data {
-		if offset+len(data[0]) > PAGESIZE {
+		if offset+len(data[i]) > PAGESIZE {
 			//create new page and write old page
-			binary.LittleEndian.PutUint64(buf[0:8], pgNum)
+			binary.LittleEndian.PutUint64(buf[0:8], uint64(pgNum))
 			binary.LittleEndian.PutUint16(buf[8:10], uint16(rows)+rowNums)
 			checksum := md5.Sum(buf[26:])
 			copy(buf[10:26], checksum[:])
@@ -191,7 +210,7 @@ func (b *BufferPoolManager) InsertData(pageid PageID, data [][]byte) (uint64, er
 			rows = 0
 			pgNum += 1
 			buf = [PAGESIZE]byte{}
-			offset = 18
+			offset = 26
 		}
 		copy(buf[offset:], data[i])
 		offset += len(data[0])
--- internal/specs.md
+++ internal/specs.md
@@ -3,4 +3,4 @@
 - bytes 0-8 = page number
 - bytes 8-10 = number of rows
 - bytes 10-26 = checksum
-- bytes 25-PAGESIZE = data
+- bytes 26-PAGESIZE = data
